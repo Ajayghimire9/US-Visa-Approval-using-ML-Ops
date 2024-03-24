14486e5c81a5525ab16b6bfd7b56971e302456a0
Ajayghimire9 <ajayghimire42@gmail.com>
2024-03-24 23:08:18 +0100

    exceptions added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 """
 =======
 # How to run?
->>>>>>> f7d53a0e2648b32865b9b7674c2c7cb803c4b2a4
+
 
 ''' bash
 conda create -n visa python=3.8 -y
@@ -24,4 +24,4 @@ pip install -r requirements.txt
 <<<<<<< HEAD
 =======
 pip install -r requirements.txt
->>>>>>> f7d53a0e2648b32865b9b7674c2c7cb803c4b2a4
+
--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,17 +1,21 @@
-import logging
 import os
-
 from datetime import datetime
-from from_root import from_root
+
+# Function to get the root directory of the project
+def get_root_directory():
+    return os.path.dirname(os.path.abspath(__file__))
+
+import logging
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_dir = 'logs'
+log_dir = os.path.join(get_root_directory(), 'logs')  # Define the full path to the log directory
 
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
-
+# Create the directory if it doesn't exist
 os.makedirs(log_dir, exist_ok=True)
 
+# Construct the path to the log file using the log directory
+logs_path = os.path.join(log_dir, LOG_FILE)
 
 logging.basicConfig(
     filename=logs_path,
