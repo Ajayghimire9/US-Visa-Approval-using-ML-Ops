af5fac968a95fbc6e412b7397f4bdcb134201837
Ajayghimire9 <ajayghimire42@gmail.com>
2024-04-03 12:50:01 +0200

    validation added

--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -3,10 +3,14 @@ from us_visa.exception import USvisaException
 from us_visa.logger import logging
 
 from us_visa.componenets.data_ingestion import DataIngestion
+from us_visa.componenets.data_validation import DataValidation
 
-from us_visa.entity.config_entity import (DataIngestionConfig) 
 
-from us_visa.entity.artifact_entity import (DataIngestionArtifact)
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig) 
+
+from us_visa.entity.artifact_entity import (DataIngestionArtifact, 
+                                            DataValidationArtifact)
 
 
 class TrainPipeline:
@@ -34,12 +38,39 @@ class TrainPipeline:
         
 
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+        
+
+
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
 
         
         except Exception as e:
